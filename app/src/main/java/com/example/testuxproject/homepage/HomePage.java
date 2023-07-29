d7bcb4489ad4fb4860c74c69c87e3a2d35afa67f
rarepompom <reginafaustine08@gmail.com>
2023-07-29 16:47:20 +0700

    border gradient xml

--- app/src/main/java/com/example/testuxproject/homepage/HomePage.java
+++ app/src/main/java/com/example/testuxproject/homepage/HomePage.java
@@ -1,20 +1,11 @@
 package com.example.testuxproject.homepage;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.CompositePageTransformer;
-import androidx.viewpager2.widget.MarginPageTransformer;
-import androidx.viewpager2.widget.ViewPager2;
-
 import android.animation.AnimatorInflater;
 import android.animation.AnimatorSet;
 import android.content.Intent;
-import android.graphics.drawable.Icon;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Gravity;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
