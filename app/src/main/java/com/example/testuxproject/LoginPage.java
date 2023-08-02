5d72f72c0eacd7d4daadb36da7057a95c4f5d3d7
Paul Calvin <paulcalvin03@gmail.com>
2023-08-03 00:18:01 +0700

    login activity border updated

--- app/src/main/java/com/example/testuxproject/LoginPage.java
+++ app/src/main/java/com/example/testuxproject/LoginPage.java
@@ -1,6 +1,7 @@
 package com.example.testuxproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.content.res.ColorStateList;
@@ -12,13 +13,13 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.text.Editable;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.testuxproject.homepage.HomePage;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginPage extends AppCompatActivity {
-
 
 
     @Override
@@ -34,6 +35,8 @@ public class LoginPage extends AppCompatActivity {
         TextInputEditText password_txt = findViewById(R.id.password_txt);
         TextInputEditText email_txt = findViewById(R.id.email_txt);
         Button buttonToLogin = findViewById(R.id.buttonToLogin);
+        TextView error_email = findViewById(R.id.errorEmail);
+        TextView error_password = findViewById(R.id.errorPassword_txt);
 
         buttonToLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,24 +51,25 @@ public class LoginPage extends AppCompatActivity {
 
                 if (email.isEmpty()) {
                     isValid = false;
-                    layoutEmail.setError("Email must be filled!");
+                    error_email.setText("Email must be filled!");
+                    error_email.setVisibility(View.VISIBLE);
                 }else if(!email.contains("@")){
                     isValid = false;
-                    layoutEmail.setError("Email must contain '@'");
+                    error_email.setText("Email must contain @");
+                    error_email.setVisibility(View.VISIBLE);
                 }else if(!email.endsWith(".com")){
                     isValid = false;
-                    layoutEmail.setError("Email must end with .com");
-                }
-                else {
-                    layoutEmail.setError(null);
-                }
-
-                if (password.isEmpty()) {
+                    error_email.setText("Email must ends with .com");
+                    error_email.setVisibility(View.VISIBLE);
+                }else if (password.isEmpty()) {
                     isValid = false;
-                    layoutPassword.setError("Password must be filled!");
+                    error_email.setVisibility(View.INVISIBLE);
+                    error_password.setText("Password must be filled!");
+                    error_password.setVisibility(View.VISIBLE);
                 }else if(password.length() < 8){
                     isValid = false;
-                    layoutPassword.setError("Password must contain at least 8 characters");
+                    error_password.setText("Password must contain at least 8 characters");
+                    error_password.setVisibility(View.VISIBLE);
                 }
                 else {
                     layoutPassword.setError(null);
@@ -78,7 +82,6 @@ public class LoginPage extends AppCompatActivity {
                 }
             }
         });
-
 
     }
 }
